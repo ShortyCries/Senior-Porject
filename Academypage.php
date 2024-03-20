469088b7c729bb676e539bac0fe708bbff8ff2b8
mohamad khalil <mohammadbassamkhalil@gmail.com>
2024-03-20 10:32:29 +0200

    final login

---
+++ Academypage.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+echo 'academy  page <br>';
+echo "hi " . $_SESSION['email'] . " by";
---
+++ CoachPage.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+echo 'coach page <br>';
+echo "hi " . $_SESSION['email'] . " by";
+?>
