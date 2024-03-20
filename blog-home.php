52269cc6a3cda5f377664c3f93355222daab2801
hossam laila <hossamemad2011@hotmail.com>
2024-03-20 13:19:56 +0200

    instagram icon inserted

--- blog-home.php
+++ blog-home.php
@@ -42,7 +42,7 @@
 						<div class="col-lg-6 col-sm-6 col-6 header-top-right">
 							<div class="header-social">
 								<a href="https://www.facebook.com/profile.php?id=100072237932656" target="_blank"><i class="fa fa-facebook"></i></a>
-								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
 							</div>
 						</div>
 					</div>
