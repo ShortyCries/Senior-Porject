103bd56747f235d3839f6a1c1d3e6217f71493b7
hossam laila <hossamemad2011@hotmail.com>
2024-04-16 14:44:41 +0300

    removed old player design

--- academy-Players.php
+++ academy-Players.php
@@ -10,7 +10,6 @@ $result = $pdo->query($query);
 
 $r = $result->rowCount();
 
-echo $r;
 
 
 ?>
@@ -193,187 +192,15 @@ echo $r;
     <div class="mybackground-img2">
         <h2 style="text-align: center; color: orange;">Players</h2>
 
-        <div class="container">
-            <div class="justify-content-center row">
-                <div class="col-lg-12">
-                    <div class="candidate-list-widgets mb-4">
-                        <form action="#" class="">
-                            <div class="g-2 row">
-                                <div class="col-lg-3">
-                                    <div class="filler-job-form">
-                                        <i class="uil uil-briefcase-alt"></i><input id="exampleFormControlInput1" placeholder="Player's Name. " type="search" class="form-control filler-job-input-box form-control" />
-                                    </div>
-                                </div>
-                                <div class="col-lg-3">
-                                    <div class="filler-job-form">
-                                        <i class="uil uil-location-point"></i>
-                                        <select class="form-select selectForm__inner" data-trigger="true" name="choices-single-location" id="choices-single-location" aria-label="Default select example">
-                                            <option value="AF">Beirut</option>
-                                            <option value="AX">Saida</option>
-                                            <option value="AL">Sour</option>
-                                            <option value="AL">Tripoli</option>
-                                            <option value="AL">Bekaa</option>
-
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-lg-3">
-                                    <div class="filler-job-form">
-                                        <i class="uil uil-clipboard-notes"></i>
-                                        <select class="form-select selectForm__inner" data-trigger="true" name="choices-single-categories" id="choices-single-categories" aria-label="Default select example">
-                                            <option value="4">Football</option>
-                                            <option value="1">Bascketball</option>
-                                            <option value="3">Tennis</option>
-                                            <option value="5">Volleyball</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-lg-3">
-                                    <div>
-                                        <a class="btn btn-primary" href="#"><i class="uil uil-filter"></i> Filter</a><a class="btn btn-success ms-2" href="#"><i class="uil uil-cog"></i> Advance</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="align-items-center row">
-                        <div class="col-lg-8">
-                            <div class="mb-3 mb-lg-0">
-                                <h6 class="fs-16 mb-0">Showing 1 – 8 of 11 results</h6>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="candidate-list-widgets">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="selection-widget">
-                                            <select class="form-select" data-trigger="true" name="choices-single-filter-orderby" id="choices-single-filter-orderby" aria-label="Default select example">
-                                                <option value="df">Default</option>
-                                                <option value="ne">Newest</option>
-                                                <option value="od">Oldest</option>
-                                                <option value="rd">Random</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="selection-widget mt-2 mt-lg-0">
-                                            <select class="form-select" data-trigger="true" name="choices-candidate-page" id="choices-candidate-page" aria-label="Default select example">
-                                                <option value="df">All</option>
-                                                <option value="ne">8 per Page</option>
-                                                <option value="ne">12 per Page</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="candidate-list">
-                        <!-- card -->
-
-                        <?php
-
-
-                        for ($i = 0; $i < $r; $i++) {
-                            $row = $result->fetch(PDO::FETCH_NUM);
-
-                            echo '
-                    <div class="candidate-list-box card mt-4">
-                        <div class="p-4 card-body">
-                            <div class="align-items-center row">
-                                <div class="col-auto">
-                                    <div class="candidate-list-images">
-                                        <a href="#"><img src="img/profiletest.jpg" alt="" class="avatar-md img-thumbnail rounded-circle" /></a>
-                                    </div>
-                                </div>
-                                <div class="col-lg-5">
-                                    <div class="candidate-list-content mt-3 mt-lg-0">
-                                        <h5 class="fs-19 mb-0">
-                                        ';
-                            echo "
-                                            <a class=\"primary-link\" href=\"#\">{$row[0]}</a>
-                                            ";
-                            echo '
-                                        </h5>
-                                        <p class="text-muted mb-2">Get his sport</p>
-                                        <ul class="list-inline mb-0 text-muted">
-                                            <li class="list-inline-item"><i class="mdi mdi-map-marker"></i> $get his sport</li><br>
-                                            <li class="list-inline-item"><i class="mdi mdi-wallet"></i> $get his class</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="mt-2 mt-lg-0 d-flex flex-wrap align-items-start gap-1">
-                                    ';
-                            echo "
-                                        <p style=\"font-size: 16px;\">{$row[3]}</p>
-                                        ";
-                            echo '
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="favorite-icon">
-                                <a href="#"> <i></i><i class="mdi mdi-account-plus fs-18"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- card -->
-                    ';
-                        }
-                        ?>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="mt-4 pt-2 col-lg-12">
-                    <nav aria-label="Page navigation example">
-                        <div class="pagination job-pagination mb-0 justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link" tabindex="-1" href="#"><i class="mdi mdi-chevron-double-left fs-15"></i></a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">4</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#"><i class="mdi mdi-chevron-double-right fs-15"></i></a>
-                            </li>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-
-
-
-
-
-
     </div>
+
+        
 
     <div class="mybackground-img2">
-        <div class="container">
-            <div class="row mt-5">
-                <div class="col">
 
-                    <div class="card-body">
-
-
-
-
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
 
     </div>
-    </div>
+
 
 
 
