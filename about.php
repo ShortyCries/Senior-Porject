98aef3cd2f2450a7ddaa90f9fc5b1e38f9ae21a4
mohamad khalil <mohammadbassamkhalil@gmail.com>
2024-05-26 14:05:59 +0300

    pic about us :happy emoji:

--- about.php
+++ about.php
@@ -1,173 +1,172 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
 
-	<!DOCTYPE html>
-	<html lang="zxx" class="no-js">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="colorlib">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Travel</title>
 
-	<head>
-		<!-- Mobile Specific Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
-		<!-- Author Meta -->
-		<meta name="author" content="colorlib">
-		<!-- Meta Description -->
-		<meta name="description" content="">
-		<!-- Meta Keyword -->
-		<meta name="keywords" content="">
-		<!-- meta character set -->
-		<meta charset="UTF-8">
-		<!-- Site Title -->
-		<title>Travel</title>
-
-		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-		<!--
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+	<!--
 			CSS
 			============================================= -->
-		<link rel="stylesheet" href="css/linearicons.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/magnific-popup.css">
-		<link rel="stylesheet" href="css/jquery-ui.css">
-		<link rel="stylesheet" href="css/nice-select.css">
-		<link rel="stylesheet" href="css/animate.min.css">
-		<link rel="stylesheet" href="css/owl.carousel.css">
-		<link rel="stylesheet" href="css/main.css">
-	</head>
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
 
-	<body>
+<body>
 
-		<header id="header">
-			<div class="header-top">
-				<div class="container">
-					<div class="row align-items-center">
-						<div class="col-lg-6 col-sm-6 col-6 header-top-left">
-						</div>
-						<div class="col-lg-6 col-sm-6 col-6 header-top-right">
-							<div class="header-social">
-								<a href="https://www.facebook.com/profile.php?id=100072237932656" target="_blank"><i class="fa fa-facebook"></i></a>
-								<a href="https://www.instagram.com/mhmdkhalil_._/" target="_blank"><i class="fa fa-instagram"></i></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="container main-menu">
-				<div class="row align-items-center justify-content-between d-flex">
-					<div id="logo">
-						<a href="index.php"><img style="height: 30px; width: 145px;" src="img/2-1.png" alt="" title="" /></a>
-						
-					</div>
-					<p style="border-left: solid; padding-left: 30px;" class="u-align-center u-text u-text-default u-text-1"></p>
-					
-					<nav id="nav-menu-container">
-						<ul class="nav-menu">
-							<li><a href="index.php">Home</a></li>
-							
-							<li><a href="about.php">About us</a></li>
-							<li><a href="contact.php">Contact</a></li>
-
-							</li>
-						</ul>
-					</nav><!-- #nav-menu-container -->
-
-				</div>
-			</div>
-		</header><!-- #header -->
-
-
-		<!-- start banner Area -->
-		<section class="about-banner relative">
-			<div class="overlay overlay-bg"></div>
-			<div class="container">
-				<div class="row d-flex align-items-center justify-content-center">
-					<div class="about-content col-lg-12">
-						<h1 class="text-white">
-							About Us
-						</h1>
-						<p class="text-white link-nav"><a href="index.php">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="about.php"> About Us</a></p>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End banner Area -->
-
-		<!-- Start about-info Area -->
-		<section class="about-info-area section-gap">
+	<header id="header">
+		<div class="header-top">
 			<div class="container">
 				<div class="row align-items-center">
-					<div class="col-lg-6 info-left">
-						<img class="img-fluid" src="img/about/info-img.jpg" alt="">
+					<div class="col-lg-6 col-sm-6 col-6 header-top-left">
 					</div>
-					<div class="col-lg-6 info-right">
-						<h6>About Us</h6>
-						<h1></h1>
-						<p></p>
+					<div class="col-lg-6 col-sm-6 col-6 header-top-right">
+						<div class="header-social">
+							<a href="https://www.facebook.com/profile.php?id=100072237932656" target="_blank"><i class="fa fa-facebook"></i></a>
+							<a href="https://www.instagram.com/mhmdkhalil_._/" target="_blank"><i class="fa fa-instagram"></i></a>
+						</div>
 					</div>
 				</div>
 			</div>
-		</section>
-		<!-- End about-info Area -->
+		</div>
+		<div class="container main-menu">
+			<div class="row align-items-center justify-content-between d-flex">
+				<div id="logo">
+					<a href="index.php"><img style="height: 30px; width: 145px;" src="img/2-1.png" alt="" title="" /></a>
 
-		<!-- Start price Area -->
+				</div>
+				<p style="border-left: solid; padding-left: 30px;" class="u-align-center u-text u-text-default u-text-1"></p>
 
-		<!-- End price Area -->
+				<nav id="nav-menu-container">
+					<ul class="nav-menu">
+						<li><a href="index.php">Home</a></li>
+
+						<li><a href="about.php">About us</a></li>
+						<li><a href="contact.php">Contact</a></li>
+
+						</li>
+					</ul>
+				</nav><!-- #nav-menu-container -->
+
+			</div>
+		</div>
+	</header><!-- #header -->
 
 
-		<!-- Start other-issue Area -->
+	<!-- start banner Area -->
+	<section class="about-banner relative">
+		<div class="overlay overlay-bg"></div>
+		<div class="container">
+			<div class="row d-flex align-items-center justify-content-center">
+				<div class="about-content col-lg-12">
+					<h1 class="text-white">
+						About Us
+					</h1>
+					<p class="text-white link-nav"><a href="index.php">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="about.php"> About Us</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End banner Area -->
 
-		<!-- End other-issue Area -->
+	<!-- Start about-info Area -->
+	<section class="about-info-area section-gap">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6 info-left">
+					<img class="img-fluid" src="img/d3c6a4f5-1690-4565-83a8-8288482171b9.jpg" alt="">
+				</div>
+				<div class="col-lg-6 info-right">
+					<h6>We are humble and hardworking engineers looking to make a diffrence in the world</h6>
+					<h1></h1>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End about-info Area -->
 
-		<!-- Start testimonial Area -->
-		<!-- End testimonial Area -->
-		<script>
-			function logoutAlert() {
-				// Show the confirmation dialog and store the result
-				var result = window.confirm("Are you sure you want to Logout?");
+	<!-- Start price Area -->
 
-				// Check if the user clicked "OK" or "Cancel"
-				if (result) {
-					// If the user clicked "OK", redirect to 'index.php'
-					window.location.href = 'logout.php';
-				} else {
-					// If the user clicked "Cancel", do nothing or perform any other action
-					return;
-				}
+	<!-- End price Area -->
+
+
+	<!-- Start other-issue Area -->
+
+	<!-- End other-issue Area -->
+
+	<!-- Start testimonial Area -->
+	<!-- End testimonial Area -->
+	<script>
+		function logoutAlert() {
+			// Show the confirmation dialog and store the result
+			var result = window.confirm("Are you sure you want to Logout?");
+
+			// Check if the user clicked "OK" or "Cancel"
+			if (result) {
+				// If the user clicked "OK", redirect to 'index.php'
+				window.location.href = 'logout.php';
+			} else {
+				// If the user clicked "Cancel", do nothing or perform any other action
+				return;
 			}
-		</script>
-		<!-- start footer Area -->
-		<footer class="footer-area section-gap">
-			<div class="container">
+		}
+	</script>
+	<!-- start footer Area -->
+	<footer class="footer-area section-gap">
+		<div class="container">
 
-				
 
-				<div class="row footer-bottom d-flex justify-content-between align-items-center">
-					<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>
-							document.write(new Date().getFullYear());
-						</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-					<div class="col-lg-4 col-sm-12 footer-social">
-						<a href="https://www.facebook.com/profile.php?id=100072237932656" target="_blank"><i class="fa fa-facebook"></i></a>
-						<a href="https://www.instagram.com/mhmdkhalil_._/" target="_blank"><i class="fa fa-instagram"></i></a>
-					</div>
+
+			<div class="row footer-bottom d-flex justify-content-between align-items-center">
+				<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					Copyright &copy;<script>
+						document.write(new Date().getFullYear());
+					</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+				<div class="col-lg-4 col-sm-12 footer-social">
+					<a href="https://www.facebook.com/profile.php?id=100072237932656" target="_blank"><i class="fa fa-facebook"></i></a>
+					<a href="https://www.instagram.com/mhmdkhalil_._/" target="_blank"><i class="fa fa-instagram"></i></a>
 				</div>
 			</div>
-		</footer>
-		<!-- End footer Area -->
+		</div>
+	</footer>
+	<!-- End footer Area -->
 
-		<script src="js/vendor/jquery-2.2.4.min.js"></script>
-		<script src="js/popper.min.js"></script>
-		<script src="js/vendor/bootstrap.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-		<script src="js/jquery-ui.js"></script>
-		<script src="js/easing.min.js"></script>
-		<script src="js/hoverIntent.js"></script>
-		<script src="js/superfish.min.js"></script>
-		<script src="js/jquery.ajaxchimp.min.js"></script>
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<script src="js/jquery.nice-select.min.js"></script>
-		<script src="js/owl.carousel.min.js"></script>
-		<script src="js/mail-script.js"></script>
-		<script src="js/main.js"></script>
-	</body>
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/easing.min.js"></script>
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mail-script.js"></script>
+	<script src="js/main.js"></script>
+</body>
 
-	</html>
+</html>
